c40210de30e89483ae0d6696c6534ba76bf7c954
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2025-03-06 11:31:47 +0530

    code

--- src/Data/achievements.ts
+++ src/Data/achievements.ts
@@ -1,37 +1,75 @@
 const getAchievements = async () => {
   return {
-    heading: "See what others are achieving through learning",
-    contents: [
-      {
-        heading: "StackOverflow",
-        description: "37,076 responses collected",
-        image: {
-          url: "https://cms-images.udemycdn.com/96883mtakkm8/2PBcNgsQa3SvYWklkiN27r/5b8707cc79c8cae5774d5eb3b88b4001/logo_stackoverflow.svg",
+    "heading": "See what others are achieving through learning",
+    "contents": [
+        {
+            "labelText": "THIRD_PARTY",
+            "description": "<p>Udemy was rated the <b>most popular online course or certification program</b> for learning how to code according to <a href=' https://survey.stackoverflow.co/2023/#section-learning-to-code-online-course-platforms-to-learn-how-to-code' class='link' rel='noreferrer' target='_blank'>StackOverflow&rsquo;s 2023 Developer survey</a>.</p>",
+            "contentUrl": "https://www.udemy.com/topic/web-development/",
+            "contentUrlText": "<p>View Web Development courses</p>",
+            "contents": [
+                {
+                    "heading": "StackOverflow",
+                    "description": "<p>37,076 responses collected</p>",
+                    "image": {
+                        "url": "https://cms-images.udemycdn.com/96883mtakkm8/2PBcNgsQa3SvYWklkiN27r/5b8707cc79c8cae5774d5eb3b88b4001/logo_stackoverflow.svg"
+                    }
+                }
+            ],
+            "trackingId": "StackOverflow_Testimonial_LOHP"
         },
-      },
-      {
-        heading: "Alvin Lim",
-        description: "Technical Co-Founder, CTO at Dimensional",
-        image: {
-          url: "https://cms-images.udemycdn.com/96883mtakkm8/1Djz6c0gZLaCG5SQS3PgUY/54b6fb8c85d8da01da95cbb94fa6335f/Alvin_Lim.jpeg",
+        {
+            "labelText": "COURSE_REVIEW",
+            "description": "<p>Udemy was truly <b>a game-changer and a great guide</b> for me as we brought Dimensional to life.</p>",
+            "contentUrl": "https://www.udemy.com/course/ios-13-app-development-bootcamp/",
+            "contentUrlText": "<p>View this iOS &amp; Swift course</p>",
+            "contents": [
+                {
+                    "heading": "Alvin Lim",
+                    "description": "<p>Technical Co-Founder, CTO at Dimensional</p>",
+                    "image": {
+                        "url": "https://cms-images.udemycdn.com/96883mtakkm8/1Djz6c0gZLaCG5SQS3PgUY/54b6fb8c85d8da01da95cbb94fa6335f/Alvin_Lim.jpeg"
+                    }
+                }
+            ],
+            "trackingId": "Alvin_Lim_Testimonial_LOHP"
         },
-      },
-      {
-        heading: "William A. Wachlin",
-        description: "Partner Account Manager at Amazon Web Services",
-        image: {
-          url: "https://cms-images.udemycdn.com/96883mtakkm8/6dT7xusLHYoOUizXeVqgUk/4317f63fe25b2e07ad8c70cda641014b/William_A_Wachlin.jpeg",
+        {
+            "labelText": "COURSE_REVIEW",
+            "description": "<p>Udemy gives you the ability to be persistent. I learned exactly what I needed to know in the real world. It helped me sell myself to <b>get a new role.</b></p>",
+            "contentUrl": "https://www.udemy.com/course/aws-certified-developer-associate-dva-c01/",
+            "contentUrlText": "<p>View this AWS course</p>",
+            "contents": [
+                {
+                    "heading": "William A. Wachlin",
+                    "description": "<p>Partner Account Manager at Amazon Web Services</p>",
+                    "image": {
+                        "url": "https://cms-images.udemycdn.com/96883mtakkm8/6dT7xusLHYoOUizXeVqgUk/4317f63fe25b2e07ad8c70cda641014b/William_A_Wachlin.jpeg"
+                    }
+                }
+            ],
+            "trackingId": "Will_W_Testimonial_LOHP"
         },
-      },
-      {
-        heading: "Ian Stevens",
-        description: "Head of Capability Development, North America at Publicis Sapient",
-        image: {
-          url: "https://cms-images.udemycdn.com/96883mtakkm8/4w9dYD4F64ibQwsaAB01Z4/c4610e9b1ac65589d8b1374ad10714e2/Ian_Stevens.png",
-        },
-      },
+        {
+            "labelText": "CASE_STUDY",
+            "description": "<p>With Udemy Business employees were able to marry the two together, technology and consultant soft skills... to help <b>drive their careers forward.</b></p>",
+            "contentUrl": "https://business.udemy.com/case-studies/publicis-sapient/?ref=Ian_Stevens_Testimonial_LOHP",
+            "contentUrlText": "<p>Read full story</p>",
+            "contents": [
+                {
+                    "heading": "Ian Stevens",
+                    "description": "<p>Head of Capability Development, North America at Publicis Sapient</p>",
+                    "image": {
+                        "url": "https://cms-images.udemycdn.com/96883mtakkm8/4w9dYD4F64ibQwsaAB01Z4/c4610e9b1ac65589d8b1374ad10714e2/Ian_Stevens.png"
+                    }
+                }
+            ],
+            "trackingId": "Ian_Stevens_Testimonial_LOHP"
+        }
     ],
-  };
-};
+    "trackingId": "LOHP Testimonials"
+}
+}
+
 
 export default getAchievements;
--- src/components/AchievementSection.tsx
+++ src/components/AchievementSection.tsx
@@ -1,18 +1,53 @@
 import { useEffect, useState } from "react";
-import getAchievements from "../Data/achievements";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const AchievementSection = () => {
-  interface Achievement {
-    image: { url: string };
-    heading: string;
-    description: string;
-  }
+interface Achievement {
+  image: { url: string };
+  heading: string;
+  description: string;
+}
 
+const getAchievements = async (): Promise<{ heading: string; contents: Achievement[] }> => {
+  return {
+    heading: "See what others are achieving through learning",
+    contents: [
+      {
+        heading: "StackOverflow",
+        description: "37,076 responses collected",
+        image: {
+          url: "https://cms-images.udemycdn.com/96883mtakkm8/2PBcNgsQa3SvYWklkiN27r/5b8707cc79c8cae5774d5eb3b88b4001/logo_stackoverflow.svg"
+        }
+      },
+      {
+        heading: "Alvin Lim",
+        description: "Technical Co-Founder, CTO at Dimensional",
+        image: {
+          url: "https://cms-images.udemycdn.com/96883mtakkm8/1Djz6c0gZLaCG5SQS3PgUY/54b6fb8c85d8da01da95cbb94fa6335f/Alvin_Lim.jpeg"
+        }
+      },
+      {
+        heading: "William A. Wachlin",
+        description: "Partner Account Manager at Amazon Web Services",
+        image: {
+          url: "https://cms-images.udemycdn.com/96883mtakkm8/6dT7xusLHYoOUizXeVqgUk/4317f63fe25b2e07ad8c70cda641014b/William_A_Wachlin.jpeg"
+        }
+      },
+      {
+        heading: "Ian Stevens",
+        description: "Head of Capability Development, North America at Publicis Sapient",
+        image: {
+          url: "https://cms-images.udemycdn.com/96883mtakkm8/4w9dYD4F64ibQwsaAB01Z4/c4610e9b1ac65589d8b1374ad10714e2/Ian_Stevens.png"
+        }
+      }
+    ]
+  };
+};
+
+const AchievementSection: React.FC = () => {
   const [data, setData] = useState<{ heading: string; contents: Achievement[] }>({
     heading: "",
-    contents: [],
+    contents: []
   });
 
   useEffect(() => {
